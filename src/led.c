e5bee3bd23961c14b4481e0ad54681357921b850
Sunday Adelodun <adelodunolaoluwa@yahoo.com>
2026-05-30 14:22:57 +0100

    feat: initial project setup with CI/CD pipeline
    - Add LED module (src/led.c, include/led.h) with unit tests (tests/test_led.c)
    - Add Makefile for building and running tests
    - Add GitHub Actions CI/CD workflow to build and run tests on push
    - Add .gitignore and README

---
+++ src/led.c
@@ -0,0 +1,12 @@
+#include "led.h"
+#include <stdio.h>
+
+void led_on(void)
+{
+    printf("LED ON\n");
+}
+
+void led_off(void)
+{
+    printf("LED OFF\n");
+}
---
+++ tests/test_led.c
@@ -0,0 +1,9 @@
+#include "led.h"
+
+int main(void)
+{
+    led_on();
+    led_off();
+
+    return 0;
+}
